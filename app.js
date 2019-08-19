3421559678d6f731ae9e9f2a5569af794854f2ce
Allwyen <allwyen27@gmail.com>
2019-08-19 21:59:37 +0530

    updated

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Express = require("express");
-
+var bodyParser = require('body-parser');
 var app = new Express();
 
 app.set('view engine','ejs');
 
 app.use(Express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.render('index');
@@ -16,6 +18,18 @@ app.get('/login',(req,res)=>{
 
 app.get('/register',(req,res)=>{
     res.render('register');
+});
+
+app.post('/readlogin',(req,res)=>{
+    var items=req.body;
+    res.render('readlogin',{item:items});
+    //res.send(req.body);
+});
+
+app.post('/readregister',(req,res)=>{
+    var items=req.body;
+    res.render('readregister',{item:items});
+    //res.send(req.body);
 });
 
 app.listen(process.env.PORT || 3456,()=>{
